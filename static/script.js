31c8b3150fd8e7a55a9e8de0150478b60b419464
Brandon Hinds <brandonhinds@13gmail.com>
2019-03-29 14:47:28 -0500

    Single graph added to compare multiple algorithms

--- static/script.js
+++ static/script.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
 			}
 
 			$(".wrapper").append('<canvas id="myChart'+ chartIndex +'" height="300px" width="300px"></canvas>');
+			$(".compare").append('<input type="checkbox" class="select" value="'+ k +'"/><p class="selectText">'+k+'</p>');
 			var ctx = document.getElementById('myChart'+chartIndex);
 			var myLineChart = new Chart(ctx, {
 			    type: 'line',
@@ -68,5 +69,68 @@ $(document).ready(function(){
 			});
 			chartIndex = chartIndex + 1;
 		}
+	});
+
+	$("#compButton").click(function(){
+		var selected = [];
+		var newlabel = [];
+
+		$(".select:checked").each(function(ind, val){
+			var vals = [];
+			var labs = [];
+
+			for(var i=0; i < results[val.value].length; i++){
+				labs.push(results[val.value][i][0]);
+				vals.push(results[val.value][i][1]);
+			}
+
+			newlabel = labs;
+
+			obj = {
+				label: val.value,
+		    	backgroundColor: colors[ind % colors.length],
+		    	borderColor: colors[ind % colors.length],
+		    	data: vals,
+		    	fill: false,
+				borderWidth: 1
+		    }
+
+		    selected.push(obj);
+		});
+
+		var ctx = document.getElementById('compChart');
+		var myLineChart = new Chart(ctx, {
+		    type: 'line',
+		    data: {
+		    	labels: newlabel,
+		    	datasets: selected
+		    },
+		    options: {
+			    responsive: false,
+			    maintainAspectRatio: false,
+			    title: {
+					display: true,
+					text: 'Comparison of Performace'
+				},
+			    scales: {
+			    	xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: '# of elements (n)'
+						}
+					}],
+			        yAxes: [{
+			        	scaleLabel:{
+			        		display: true,
+			        		labelString: 'Time'
+			        	},
+			            ticks: {
+			                beginAtZero:false
+			            }
+			        }]
+			    }
+			}
+		});
 	});
 });
